432e9d20eef8ad3def0d3c0a198c96cf410fac71
placki-w <placki@gmail.com>
2025-04-15 20:06:30 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
-"fmt"
+	"fmt"
+	"strings"
 )
 
 func main() {
 	fmt.Printf("Hello, World!")
+
+}
+
+func cleanInput(text string) []string {
+	var result []string
+	words := strings.Fields(text)
+	for _, word := range words {
+		word = strings.ToLower(word)
+		result = append(result, word)
+	}
+	return result
 }
